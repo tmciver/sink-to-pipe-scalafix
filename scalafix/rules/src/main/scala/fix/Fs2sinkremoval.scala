7475c4fbea8a3e490eec907b193984842d7d18ca
Tim McIver <tim@timmciver.com>
2019-01-16 16:14:59 -0500

    Now substituting return type `Pipe[F, A, Unit]` in place of `Sink[F, A]`.

--- scalafix/rules/src/main/scala/fix/Fs2sinkremoval.scala
+++ scalafix/rules/src/main/scala/fix/Fs2sinkremoval.scala
@@ -6,10 +6,20 @@ import scala.meta._
 class Fs2sinkremoval extends SemanticRule("Fs2sinkremoval") {
 
   override def fix(implicit doc: SemanticDocument): Patch = {
-    println("Tree.syntax: " + doc.tree.syntax)
-    println("Tree.structure: " + doc.tree.structure)
-    println("Tree.structureLabeled: " + doc.tree.structureLabeled)
-    Patch.empty
+    // println("Tree.syntax: " + doc.tree.syntax)
+    //println("Tree.structure: " + doc.tree.structure)
+    // println("Tree.structureLabeled: " + doc.tree.structureLabeled)
+
+    //val sinkType = SymbolMatcher.normalized("fs2/Sink/")
+    val sinkType = Type.Name("Sink")
+    doc.tree.collect {
+      case sink @ Type.Apply(Type.Name("Sink"), List(f, a)) =>
+        //println(name.pos.formatMessage("info", "Here is the Sink type."))
+        println(s"Found the Sink type and it has the following type arguments: F = $f, A = $a")
+        Patch.replaceTree(sink, s"Pipe[$f, $a, Unit]")
+    }.asPatch
+
+    //Patch.empty
   }
 
 }
