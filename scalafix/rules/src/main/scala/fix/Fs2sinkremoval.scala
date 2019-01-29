a23e222b1a495883e4ea79032720fa681ff48a96
Tim McIver <tim@timmciver.com>
2019-01-29 14:32:41 -0500

    First working version.

--- scalafix/rules/src/main/scala/fix/Fs2sinkremoval.scala
+++ scalafix/rules/src/main/scala/fix/Fs2sinkremoval.scala
@@ -7,7 +7,7 @@ class Fs2sinkremoval extends SemanticRule("Fs2sinkremoval") {
 
   override def fix(implicit doc: SemanticDocument): Patch = {
     // println("Tree.syntax: " + doc.tree.syntax)
-    //println("Tree.structure: " + doc.tree.structure)
+    println("Tree.structure: " + doc.tree.structure)
     // println("Tree.structureLabeled: " + doc.tree.structureLabeled)
 
     // Patch to replace Sink type annotation with Pipe type annotation
@@ -15,12 +15,32 @@ class Fs2sinkremoval extends SemanticRule("Fs2sinkremoval") {
       case sink @ Type.Apply(Type.Name("Sink"), List(f, a)) =>
         println(s"Found the Sink type and it has the following type arguments: F = $f, A = $a")
         Patch.replaceTree(sink, s"Pipe[$f, $a, Unit]")
+      // case q"import ..$importersnel" => {
+      //   println(importersnel)
+      //   Patch.empty
+      // }
+      case imp@Importer(Term.Name("fs2"), importees) =>
+        val sinkImportee = Importee.Name(scala.meta.Name.Indeterminate("Sink"))
+        val pipeImportee = Importee.Name(scala.meta.Name.Indeterminate("Pipe"))
+        val renamedImports = importees.map({
+          //case `sinkImportee` => pipeImportee
+          case Importee.Name(scala.meta.Name.Indeterminate(name)) => { println(s"Name: $name"); if (name == "Sink") pipeImportee else sinkImportee }
+          case x => x
+        })
+        val importerRenamed = Importer(Term.Name("fs2"), renamedImports)
+        //val importees = importers.flatmap(_.importees)
+        println(s"Importees: $importees")
+        println(s"Renamed imports: $importerRenamed")
+        Patch.replaceTree(imp, importerRenamed.toString)
+
       case _ => Patch.empty
     }.asPatch
 
-    // Patch to change fs2.Sink import to fs2.Pipe
-    val importPatch = Patch.renameSymbol(Symbol("fs2/Sink."), "fs2.Pipe")
+    typePatch
 
-    typePatch + importPatch
+    // Patch to change fs2.Sink import to fs2.Pipe
+    //val importPatch = Patch.renameSymbol(Symbol("fs2/Sink."), "fs2.Pipe")
+
+    //typePatch + importPatch
   }
 }
