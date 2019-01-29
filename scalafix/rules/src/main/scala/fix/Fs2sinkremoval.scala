64f01e7b83b76612f39181e0d8ca11f384dde9f6
Tim McIver <tim@timmciver.com>
2019-01-29 14:35:36 -0500

    Clean up and comment code.

--- scalafix/rules/src/main/scala/fix/Fs2sinkremoval.scala
+++ scalafix/rules/src/main/scala/fix/Fs2sinkremoval.scala
@@ -7,40 +7,28 @@ class Fs2sinkremoval extends SemanticRule("Fs2sinkremoval") {
 
   override def fix(implicit doc: SemanticDocument): Patch = {
     // println("Tree.syntax: " + doc.tree.syntax)
-    println("Tree.structure: " + doc.tree.structure)
+    //println("Tree.structure: " + doc.tree.structure)
     // println("Tree.structureLabeled: " + doc.tree.structureLabeled)
 
-    // Patch to replace Sink type annotation with Pipe type annotation
-    val typePatch = doc.tree.collect {
+    doc.tree.collect {
+
+      // Patch to replace Sink type annotation with Pipe type annotation
       case sink @ Type.Apply(Type.Name("Sink"), List(f, a)) =>
-        println(s"Found the Sink type and it has the following type arguments: F = $f, A = $a")
         Patch.replaceTree(sink, s"Pipe[$f, $a, Unit]")
-      // case q"import ..$importersnel" => {
-      //   println(importersnel)
-      //   Patch.empty
-      // }
+
+      // Case to fix fs2.Sink import
       case imp@Importer(Term.Name("fs2"), importees) =>
         val sinkImportee = Importee.Name(scala.meta.Name.Indeterminate("Sink"))
         val pipeImportee = Importee.Name(scala.meta.Name.Indeterminate("Pipe"))
         val renamedImports = importees.map({
-          //case `sinkImportee` => pipeImportee
+          //case `sinkImportee` => pipeImportee  // why doesn't this match work?
           case Importee.Name(scala.meta.Name.Indeterminate(name)) => { println(s"Name: $name"); if (name == "Sink") pipeImportee else sinkImportee }
           case x => x
         })
         val importerRenamed = Importer(Term.Name("fs2"), renamedImports)
-        //val importees = importers.flatmap(_.importees)
-        println(s"Importees: $importees")
-        println(s"Renamed imports: $importerRenamed")
         Patch.replaceTree(imp, importerRenamed.toString)
 
       case _ => Patch.empty
     }.asPatch
-
-    typePatch
-
-    // Patch to change fs2.Sink import to fs2.Pipe
-    //val importPatch = Patch.renameSymbol(Symbol("fs2/Sink."), "fs2.Pipe")
-
-    //typePatch + importPatch
   }
 }
