fe8860fb237cacb71501e9c82f575fff1f59eda6
Tim McIver <tim@timmciver.com>
2019-01-31 11:27:30 -0500

    Added compound import to input/output files to ensure that these would be fixed correctly.

--- scalafix/input/src/main/scala/fix/Fs2sinkremoval.scala
+++ scalafix/input/src/main/scala/fix/Fs2sinkremoval.scala
@@ -3,7 +3,7 @@ rule = Fs2sinkremoval
 */
 package fix
 
-import fs2.Sink
+import fs2.{Sink, Pure}
 
 object Fs2sinkremoval {
   private def writeSink[F[_]]: Sink[F, Byte] = ???
--- scalafix/output/src/main/scala/fix/Fs2sinkremoval.scala
+++ scalafix/output/src/main/scala/fix/Fs2sinkremoval.scala
@@ -1,6 +1,6 @@
 package fix
 
-import fs2.Pipe
+import fs2.{Pipe, Pure}
 
 object Fs2sinkremoval {
   private def writeSink[F[_]]: Pipe[F, Byte, Unit] = ???
